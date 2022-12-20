6c94c7744f7242b82e31615d98bd25254fab81e6
Nina <you@example.com>
2022-12-20 09:45:39 +0300

    Решениие задачи 1,2

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = int.Parse(Console.ReadLine()!);
+int result = ((number / 100) * 0 + number % 100);
+int itog = result / 10;
+Console.WriteLine($"{number}-> {itog}");
+if (number > 99 && number <= 999)
+{
+    Console.WriteLine("Число является трехзначным");
+}
+else
+{
+    Console.WriteLine("Число не является трехзначным");
+}
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+if (number>100 && number<= 999)
+{
+    int result = number%100;
+    int itog= result/10;
+    Console.WriteLine($"{number}-> {itog}");
+}
+if (number>1000 && number<= 9999)
+{
+    int result = number%100;
+    int itog= result/10;
+    Console.WriteLine($"{number}-> {itog}");
+}
+if (number>10000 && number<= 99999)
+{
+    int result = number%1000;
+    int itog= result/100;
+    Console.WriteLine($"{number}-> {itog}");
+}
+if (number<99)
+{
+    
+    Console.WriteLine($"Третьей цифры нет");
+}
